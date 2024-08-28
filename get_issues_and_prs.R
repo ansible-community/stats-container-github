be4b307aef58fbaba567ce27f65cf64f9167f66b
Greg Sutcliffe <github@emeraldreverie.org>
2024-08-28 11:59:29 +0100

    Actually run the loop, also cleanup old pins as we go

--- get_issues_and_prs.R
+++ get_issues_and_prs.R
@@ -3,6 +3,7 @@
 # the next 100 (by date) to index
 
 library(dplyr)
+library(pins)
 library(reticulate)
 use_virtualenv("./github-py") # local venv or in Container
 
@@ -12,13 +13,12 @@ source('/srv/docker-config/github/github.token')
 source_python('./crawl_issues_and_prs.py')
 org_repo_cfg <- config::get(file = '/srv/docker-config/github/repos.yml')
 
-
 # Set up pin boards
-meta_board <- pins::board_folder('/srv/docker-pins/github/meta')
-repo_board <- pins::board_folder('/srv/docker-pins/github/repos')
+meta_board <- board_folder('/srv/docker-pins/github/meta')
+repo_board <- board_folder('/srv/docker-pins/github/repos')
 
 # Load the full repo list
-meta_repos <- pins::pin_read(meta_board, 'repos')
+meta_repos <- pin_read(meta_board, 'repos')
 
 # Check the metadata on each repo pin and remove any repo which
 # has been parsed this week
@@ -26,12 +26,21 @@ meta_repos <- pins::pin_read(meta_board, 'repos')
 safe_pin_meta <- purrr::possibly(~{pin_meta(.x, .y)$created |> as.character()},
                                  otherwise = NA)
 meta_repos |>
-  dplyr::mutate(pin_name = glue::glue("{org}|{repo}")) |>
-  dplyr::mutate(pin_time = purrr::map_chr(pin_name, ~safe_pin_meta(repo_board, .x))) |>
+  mutate(pin_name = glue::glue("{org}|{repo}")) |>
+  mutate(pin_time = purrr::map_chr(pin_name, ~safe_pin_meta(repo_board, .x))) |>
   arrange(pin_time) |>
   filter((pin_time < Sys.time() - 60*60*24*7) | is.na(pin_time)) |>
   head(org_repo_cfg$repos_to_index) -> index_list
 
+# Remove any pins that are no longer on the list
+pin_list(repo_board) |>
+  tibble::enframe(name = NULL, value = "pin_name") |>
+  anti_join( meta_repos |> mutate(pin_name = glue::glue("{org}|{repo}")),
+             by = "pin_name") |>
+  pull(pin_name) |>
+  pin_delete(board = repo_board)
+
+# Function to actually do the work
 pin_issues_prs_comments <- function(git_org, git_repo, get_all = TRUE) {
   # Setup temporary work path
   work_dir <- fs::path_temp('gh_indexer', git_org, git_repo)
@@ -54,3 +63,6 @@ pin_issues_prs_comments <- function(git_org, git_repo, get_all = TRUE) {
   # Delete temp work path
   fs::dir_delete(work_dir)
 }
+
+# Run the function over the short-list
+purrr::walk2(index_list$org, index_list$repo, pin_issues_prs_comments)
